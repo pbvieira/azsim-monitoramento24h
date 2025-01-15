c346573c409b11a6027809f2d16b9f7bbda5f90e
DevNathanViana <oliveiranathan700@gmail.com>
2025-01-15 00:51:37 -0300

    Mudança cards Cadastro

--- react/src/components/Formularios/Card.jsx
+++ react/src/components/Formularios/Card.jsx
@@ -1,19 +1,29 @@
 /* eslint-disable react/prop-types */
-import { Card } from "./Form"
+import { useState } from "react"
+import { Card, DropdownContent, DropdownItem, Options } from "./Form.js"
 
 function CardInformacoes({ index, title1, text1, title2, text2, handleOpenModal, deleteItem, item }) {
+
+    const [booleanDropdown, setBooleanDropdown] = useState(false);
+
     return (
         <Card>
             <div>
-                <h6>{title1}: {text1} </h6>
-                <button className="btn btnExluir" onClick={() => deleteItem(item, index)}>
-                    <span className="material-symbols-outlined">
-                        delete
-                    </span>
-                </button>
+                <div>
+                    <h6>{title1}: {text1} </h6>
+                </div>
+                <Options>
+                    <button className="btn btnOptions" onClick={() => setBooleanDropdown(!booleanDropdown)}> <span className="material-symbols-outlined">
+                        more_vert
+                    </span></button>
+                    <DropdownContent show={booleanDropdown}>
+                        <DropdownItem className="btn" onClick={() => { deleteItem(item, index), setBooleanDropdown(!booleanDropdown) }}>Deletar</DropdownItem>
+                        <DropdownItem className="btn" onClick={() => { handleOpenModal(index), setBooleanDropdown(!booleanDropdown) }}>Ver mais</DropdownItem>
+                    </DropdownContent>
+                </Options>
             </div>
 
-            <div><h6>{title2}: {text2}</h6><button onClick={() => handleOpenModal(index)} className="btn btn-primary">Ver Mais</button></div>
+            <div><h6>{title2}: {text2}</h6></div>
         </Card>
     )
 }
